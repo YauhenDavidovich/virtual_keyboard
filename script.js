58ab27856b0dd2caf28853d0c723059f02371305
unknown <davidovich336@gmail.com>
2020-03-30 14:35:20 +0300

    feat: add text input field

---
+++ script.js
@@ -0,0 +1,6 @@
+// INPUT AREA
+
+const inputarea = document.createElement('inputarea');
+inputarea.classList.add('inputarea');
+inputarea.setAttribute('autofocus', '');
+document.body.append(inputarea);

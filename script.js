9ac04f36307ccb024638d4f33fbb9a89a2124c9d
Евгений Давидович <davidovichyauhen@gmail.com>
2020-04-06 21:50:03 +0300

    refactor: add const Keyboard

--- script.js
+++ script.js
@@ -7,156 +7,160 @@ document.body.append(textarea);
 
 // keyboard
 
-const keyboard = document.createElement("div");
-keyboard.classList.add("keyboard");
-document.body.append(keyboard);
+const Keyboard = { 
 
-const keyboardKeysEngShiftOff = [
-  "`",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "0",
-  "-",
-  "=",
-  "\\",
-  "backspace",
-];
-const keyboardKeysEngShiftOn = [
-  "~",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "+",
-  "|",
-  "backspace",
-];
-const keyboardKeysEng = [
-  "Tab",
-  "q",
-  "w",
-  "e",
-  "r",
-  "t",
-  "y",
-  "u",
-  "i",
-  "o",
-  "p",
-  "[",
-  "]",
-  "Del",
-  "CapsLock",
-  "a",
-  "s",
-  "d",
-  "f",
-  "g",
-  "h",
-  "j",
-  "k",
-  "l",
-  ";",
-  "'",
-  "Enter",
-  "Shift",
-  "z",
-  "x",
-  "c",
-  "v",
-  "b",
-  "n",
-  "m",
-  ",",
-  ".",
-  "?",
-  "Shift",
-  "Up",
-  "Ctrl",
-  "Win",
-  "Alt",
-  "space",
-  "Alt",
-  "Ctrl",
-  "Left",
-  "Down",
-  "Right",
-];
-const keyboardKeysRusShiftOff = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='];
-const keyboardKeysRusShiftOn = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'backspace'];
-const keyboardKeysRus = [
-  'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '/', '|',
-  'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
-  'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Shift', 'Up', 'Del',
-  'Ctrl', 'Win', 'Alt', 'space', 'Alt', 'Ctrl', 'Left', 'Down', 'Right',
-];
+  keyboardKeysEngShiftOff: [
+    "`",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "-",
+    "=",
+    "\\",
+    "backspace",
+  ],
 
-const keyboardArea = document.querySelector("keyboard");
+  keyboardKeysEngShiftOn: [
+    "~",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "+",
+    "|",
+    "backspace",
+  ],
 
-// keyboard keys
+  keyboardMainKeysEngShiftOff: [
+    "Tab",
+    "q",
+    "w",
+    "e",
+    "r",
+    "t",
+    "y",
+    "u",
+    "i",
+    "o",
+    "p",
+    "[",
+    "]",
+    "Del",
+    "CapsLock",
+    "a",
+    "s",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    ";",
+    "'",
+    "Enter",
+    "Shift",
+    "z",
+    "x",
+    "c",
+    "v",
+    "b",
+    "n",
+    "m",
+    ",",
+    ".",
+    "?",
+    "Shift",
+    "Up",
+    "Ctrl",
+    "Win",
+    "Alt",
+    "space",
+    "Alt",
+    "Ctrl",
+    "Left",
+    "Down",
+    "Right",
+  ],
 
-function createKeys(keyboardKeysShiftOn, keyboardKeys) {
-  for (let i = 0; i < keyboardKeysShiftOn.length; i++) {
-    const div = document.createElement("div");
-    div.className = "key";
-    div.innerHTML = keyboardKeysShiftOn[i];
-    keyboard.append(div);
-    if (i === 14) {
-      const divBr = document.createElement("div");
-      divBr.className = "break";
-      keyboard.append(divBr);
+  keyboardKeysRusShiftOff: ["ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "\\", "backspace"],
+
+  keyboardKeysRusShiftOn: ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', "|", "backspace"],
+  keyboardMainKeysRusShiftOff: [
+    'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'Del',
+    'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
+    'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Shift', 'Up',
+    'Ctrl', 'Win', 'Alt', 'space', 'Alt', 'Ctrl', 'Left', 'Down', 'Right',
+  ],
+  
+
+  // keyboard keys
+
+  createKeys(keyboardKeysShift, keyboardMainKeys) {
+    const keyboardMake = document.createElement("div");
+    keyboardMake.classList.add("keyboard");
+    document.body.append(keyboardMake);
+    for (let i = 0; i < keyboardKeysShift.length; i++) {
+      const div = document.createElement("div");
+      div.className = "key";
+      div.innerHTML = keyboardKeysShift[i];
+      keyboardMake.append(div);
+      if (i === 14) {
+        const divBr = document.createElement("div");
+        divBr.className = "break";
+        keyboardMake.append(divBr);
+      }
+      if (keyboardKeysShift[i] === "backspace") {
+        div.className = "key key_extra";
+      }
     }
-    if (keyboardKeysShiftOn[i] === "backspace") {
-      div.className = "key key_extra";
+    for (let i = 0; i < keyboardMainKeys.length; i++) {
+      const div = document.createElement("div");
+      div.className = "key";
+      div.innerHTML = keyboardMainKeys[i];
+      keyboardMake.append(div);
+      if (i === 13 || i === 26 || i === 39) {
+        const divBr = document.createElement("div");
+        divBr.className = "break";
+        keyboardMake.append(divBr);
+      }
+      if (
+        keyboardMainKeys[i] === "CapsLock" ||
+        keyboardMainKeys[i] === "Shift" ||
+        keyboardMainKeys[i] === "Tab" ||
+        keyboardMainKeys[i] === "Enter" ||
+        keyboardMainKeys[i] === "Win" ||
+        keyboardMainKeys[i] === "Alt"
+      ) {
+        div.className = "key key_extra";
+      }
+      if (keyboardMainKeys[i] === "space") {
+        div.className = "key key_extra-wide";
+      }
     }
   }
-  for (let i = 0; i < keyboardKeys.length; i++) {
-    const div = document.createElement("div");
-    div.className = "key";
-    div.innerHTML = keyboardKeys[i];
-    keyboard.append(div);
-    if (i === 13 || i === 26 || i === 39) {
-      const divBr = document.createElement("div");
-      divBr.className = "break";
-      keyboard.append(divBr);
-    }
-    if (
-      keyboardKeys[i] === "CapsLock" ||
-      keyboardKeys[i] === "Shift" ||
-      keyboardKeys[i] === "Tab" ||
-      keyboardKeys[i] === "Enter" ||
-      keyboardKeys[i] === "Win" ||
-      keyboardKeys[i] === "Alt"
-    ) {
-      div.className = "key key_extra";
-    }
-    if (keyboardKeys[i] === "space") {
-      div.className = "key key_extra-wide";
-    }
-  }
+
 }
 
-// Language switch
-
-if (localStorage.getItem('keyboard') === 'eng') {
-  createKeys(keyboardKeysEngShiftOff, keyboardKeysEng);
-} else if (localStorage.getItem('keyboard') === 'rus') {
-  createKeys(keyboardKeysRusShiftOff, keyboardKeysEng);
-} else createKeys(keyboardKeysEngShiftOff, keyboardKeysEng);
-
-
-
+window.addEventListener('load', () => {      
+      if (localStorage.getItem('keyboard') === 'eng') {
+        Keyboard.createKeys(keyboardKeysEngShiftOff, keyboardMainKeysEngShiftOff);
+      } else if (localStorage.getItem('keyboard') === 'rus') {
+        Keyboard.createKeys(keyboardKeysRusShiftOff, keyboardMainKeysRusShiftOff);
+      } else Keyboard.createKeys(Keyboard.keyboardKeysRusShiftOff, Keyboard.keyboardMainKeysRusShiftOff);
+      Keyboard.listenRealKeyboard()
+      Keyboard.addClickKeyboardHandler()})

4d6e72037cb9b59e5f67b8806537d54c15d9c3fe
Евгений Давидович <davidovichyauhen@gmail.com>
2020-04-10 22:15:44 +0300

    feat: add language toggle

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 // text area
-
 const textarea = document.createElement("textarea");
 textarea.classList.add("textarea");
 textarea.setAttribute("autofocus", "");
 document.body.append(textarea);
-
 // keyboard
-
 const Keyboard = {
   generateDescribe() {
-    const divDescribe = document.createElement('div');
-    let p1 = document.createElement('p');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
-    p1.className = 'describe';
-    p1.innerText = 'Клавиатура создана в операционной системе Windows';
-    p2.className = 'describe';
-    p2.innerText = 'Для переключения языка используйте клавиши: ';
-    p3.className = 'describe';
-    p3.innerText = 'Уважаемый проверяющий, отложи ,пожалуйста, проверку до 12.04.2020(с учетом просрочки дедлайна) - не успел прикрутить функционал! ';
-    document.body.append(p1);
-    document.body.append(p2);
-    document.body.append(p3);    
+    const divDescribe = document.createElement("div");
+    document.body.append(divDescribe);
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    divDescribe.className = "describe";
+    p1.innerText = "Клавиатура создана в операционной системе Windows";
+    p2.innerText = "Для переключения языка используйте клавиши: ";
+    divDescribe.append(p1);
+    divDescribe.append(p2);
   },
-
   keyboardKeysEngShiftOff: [
     "`",
     "1",
@@ -112,25 +104,171 @@ const Keyboard = {
     "Right",
   ],
 
-  keyboardKeysRusShiftOff: ["ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "\\", "backspace"],
-
-  keyboardKeysRusShiftOn: ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', "|", "backspace"],
-  keyboardMainKeysRusShiftOff: [
-    'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'Del',
-    'CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter',
-    'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Shift', 'Up',
-    'Ctrl', 'Win', 'Alt', 'space', 'Alt', 'Ctrl', 'Left', 'Down', 'Right',
+  keyboardKeysRusShiftOff: [
+    "ё",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "-",
+    "=",
+    "\\",
+    "backspace",
   ],
 
+  keyboardKeysRusShiftOn: [
+    "Ё",
+    "!",
+    '"',
+    "№",
+    ";",
+    "%",
+    ":",
+    "?",
+    "*",
+    "(",
+    ")",
+    "_",
+    "+",
+    "|",
+    "backspace",
+  ],
+  keyboardMainKeysRusShiftOff: [
+    "Tab",
+    "й",
+    "ц",
+    "у",
+    "к",
+    "е",
+    "н",
+    "г",
+    "ш",
+    "щ",
+    "з",
+    "х",
+    "ъ",
+    "Del",
+    "CapsLock",
+    "ф",
+    "ы",
+    "в",
+    "а",
+    "п",
+    "р",
+    "о",
+    "л",
+    "д",
+    "ж",
+    "э",
+    "Enter",
+    "Shift",
+    "я",
+    "ч",
+    "с",
+    "м",
+    "и",
+    "т",
+    "ь",
+    "б",
+    "ю",
+    ".",
+    "Shift",
+    "Up",
+    "Ctrl",
+    "Win",
+    "Alt",
+    "space",
+    "Alt",
+    "Ctrl",
+    "Left",
+    "Down",
+    "Right",
+  ],
 
+  keyCodes: [
+    "Backquote",
+    "Digit1",
+    "Digit2",
+    "Digit3",
+    "Digit4",
+    "Digit5",
+    "Digit6",
+    "Digit7",
+    "Digit8",
+    "Digit9",
+    "Digit0",
+    "Minus",
+    "Equal",
+    "Backslash",
+    "Backspace",
+    "Tab",
+    "KeyQ",
+    "KeyW",
+    "KeyE",
+    "KeyR",
+    "KeyT",
+    "KeyY",
+    "KeyU",
+    "KeyI",
+    "KeyO",
+    "KeyP",
+    "BracketLeft",
+    "BracketRight",
+    "Delete",
+    "CapsLock",
+    "KeyA",
+    "KeyS",
+    "KeyD",
+    "KeyF",
+    "KeyG",
+    "KeyH",
+    "KeyJ",
+    "KeyK",
+    "KeyL",
+    "Semicolon",
+    "Quote",
+    "Enter",
+    "ShiftLeft",
+    "KeyZ",
+    "KeyX",
+    "KeyC",
+    "KeyV",
+    "KeyB",
+    "KeyN",
+    "KeyM",
+    "Comma",
+    "Period",
+    "Slash",
+    "ShiftRight",
+    "ArrowUp",
+    "ControlLeft",
+    "MetaLeft",
+    "AltLeft",
+    "Space",
+    "AltRight",
+    "ControlRight",
+    "ArrowLeft",
+    "ArrowDown",
+    "ArrowRight",
+  ],
   // keyboard keys
-
   createKeys(keyboardKeysShift, keyboardMainKeys) {
     const keyboardMake = document.createElement("div");
+    keyboardMake.id = "keyboard";
+    if (keyboardKeysShift === Keyboard.keyboardKeysEngShiftOff)
+      keyboardMake.classList.add("english");
+    if (keyboardKeysShift === Keyboard.keyboardKeysRusShiftOff)
+      keyboardMake.classList.add("russian");
     keyboardMake.classList.add("keyboard");
     document.body.append(keyboardMake);
-    for (let i = 0; i < keyboardKeysShift.length; i++) {
-      const div = document.createElement("div");
+    for (let i = 0; i < keyboardKeysShift.length; i += 1) {
+      const div = document.createElement("button");
       div.className = "key";
       div.innerHTML = keyboardKeysShift[i];
       keyboardMake.append(div);
@@ -143,8 +281,8 @@ const Keyboard = {
         div.className = "key key_extra";
       }
     }
-    for (let i = 0; i < keyboardMainKeys.length; i++) {
-      const div = document.createElement("div");
+    for (let i = 0; i < keyboardMainKeys.length; i += 1) {
+      const div = document.createElement("button");
       div.className = "key";
       div.innerHTML = keyboardMainKeys[i];
       keyboardMake.append(div);
@@ -163,29 +301,76 @@ const Keyboard = {
       ) {
         div.className = "key key_extra";
       }
+      if (keyboardMainKeys[i] === "Alt" || keyboardMainKeys[i] === "Ctrl") {
+        div.className = "key key_extra alt";
+      }
       if (keyboardMainKeys[i] === "space") {
         div.className = "key key_extra-wide";
       }
     }
   },
-
-  properties: {
-    capsLock: false,
-    shift: false,
-    rus: false,
+  languageChange() {
+    const keyarea = document.getElementById("keyboard");
+    if (keyarea.classList.contains("russian")) {
+      keyarea.classList.remove("russian");
+      keyarea.parentNode.removeChild(keyarea);
+      Keyboard.createKeys(
+        Keyboard.keyboardKeysEngShiftOff,
+        Keyboard.keyboardMainKeysEngShiftOff
+      );
+      localStorage.setItem("language", "eng");
+    } else if (keyarea.classList.contains("english")) {
+      keyarea.classList.remove("english");
+      keyarea.parentNode.removeChild(keyarea);
+      Keyboard.createKeys(
+        Keyboard.keyboardKeysRusShiftOff,
+        Keyboard.keyboardMainKeysRusShiftOff
+      );
+      localStorage.setItem("language", "rus");
+    }
   },
+};
 
+if (localStorage.getItem("language") === "eng") {
+  Keyboard.createKeys(
+    Keyboard.keyboardKeysEngShiftOff,
+    Keyboard.keyboardMainKeysEngShiftOff
+  );
+} else if (localStorage.getItem("language") === "rus") {
+  Keyboard.createKeys(
+    Keyboard.keyboardKeysRusShiftOff,
+    Keyboard.keyboardMainKeysRusShiftOff
+  );
+} else
+  Keyboard.createKeys(
+    Keyboard.keyboardKeysRusShiftOff,
+    Keyboard.keyboardMainKeysRusShiftOff
+  );
+Keyboard.generateDescribe();
 
-}
+window.addEventListener("keydown", (evt) => {
+  const buttons = document.querySelectorAll("button");
+  Keyboard.keyCodes.forEach((item, index) => {
+    if (evt.code === item) {
+      console.log(index);
+      buttons[index].classList.add("active");
+    }
+    if (
+      buttons[42].classList.contains("active") &&
+      buttons[57].classList.contains("active")
+    ) {
+      Keyboard.languageChange();
+      console.log("change");
+    }
+  });
+});
 
-window.addEventListener('load', () => {
-  
-  if (localStorage.getItem('keyboard') === 'eng') {
-    Keyboard.createKeys(keyboardKeysEngShiftOff, keyboardMainKeysEngShiftOff);
-  } else if (localStorage.getItem('keyboard') === 'rus') {
-    Keyboard.createKeys(keyboardKeysRusShiftOff, keyboardMainKeysRusShiftOff);
-  } else Keyboard.createKeys(Keyboard.keyboardKeysRusShiftOff, Keyboard.keyboardMainKeysRusShiftOff);
-  Keyboard.generateDescribe();
-  Keyboard.listenPhysicalKeyboard();
-  Keyboard.clickKeyboardHandler()
-})
+window.addEventListener("keyup", (evt) => {
+  const buttons = document.querySelectorAll("button");
+  Keyboard.keyCodes.forEach((item, index) => {
+    if (evt.code === item) {
+      console.log(evt.code);
+      buttons[index].classList.remove("active");
+    }
+  });
+});
